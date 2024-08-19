40c44b45fba72f544e10d6886fde5d414b5fee94
Yahya Abad <yabad@c2r4p4.1337.ma>
2024-08-19 12:49:16 +0100

    adding a basic implementation for the Router that has a RoutingTable and a Server using composition

--- src/ma/leet/fixme/router/Router.java
+++ src/ma/leet/fixme/router/Router.java
@@ -1,3 +1,20 @@
-public class Router {
-	
+import java.nio.channels.SocketChannel;
+
+public class Router implements ClientRegistrationListener {
+	private final RoutingTable routingTable = new RoutingTable();
+	private final RouterServer routerServer;
+
+	public Router() {
+		this.routerServer = new RouterServer(this);
+	}
+
+	public void onClientConnected(SocketChannel client, int port) {
+		String id = routingTable.addClient(client);
+		String clientType = (port == RouterServer.BROKER_PORT) ? "Broker" : "Market";
+		System.out.println(clientType + " client connected with ID: " + id);
+	}
+
+	public static void main(String[] args) {
+		new Router();
+	}
 }
